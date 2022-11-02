02f69877db7983a50d97aeaf72a185a5d8c204c0
ADITYA KALYAN <79601235+Kalyan4636@users.noreply.github.com>
2022-11-02 20:24:21 +0530

    code added

---
+++ pattern3.py
@@ -0,0 +1,7 @@
+# Write a python code to print following star pattern:
+
+n = 5 
+for i in range(n): 
+   for j in range(i, n): 
+      print('*', end=' ')
+   print()
---
+++ pattern4.py
@@ -0,0 +1,11 @@
+# Write a python program to print following hill star pattern
+
+n = 5 
+for i in range(n): 
+   for j in range(i, n): 
+      print(' ', end=' ') 
+   for j in range(i):
+      print('*', end=' ')
+   for j in range(i+1):
+      print('*', end=' ')
+   print()
